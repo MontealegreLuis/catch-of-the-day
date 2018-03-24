84a14e3dd3b4f096f6993d2b6848dc7e36494b31
Luis Montealegre <montealegreluis@gmail.com>
2018-03-24 00:24:31 -0500

    chore: Create form to add a new fish

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,43 @@
+/*
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+import React from 'react';
+
+class AddFishForm extends React.Component {
+    name = React.createRef();
+    price = React.createRef();
+    status = React.createRef();
+    desc = React.createRef();
+    image = React.createRef();
+
+    createFish = event => {
+        event.preventDefault();
+        const fish = {
+            name: this.name.current.value,
+            price: parseFloat(this.price.current.value),
+            status: this.status.current.value,
+            desc: this.desc.current.value,
+            image: this.image.current.value
+        };
+        this.props.addFish(fish);
+        event.currentTarget.reset();
+    };
+
+    render() {
+        return (
+            <form className="fish-edit" onSubmit={this.createFish}>
+                <input name="name" ref={this.name} placeholder="Name" />
+                <input name="price" ref={this.price} placeholder="Price" />
+                <select name="status" ref={this.status}>
+                    <option value="available">Fresh!</option>
+                    <option value="unavailable">Sold out!</option>
+                </select>
+                <textarea name="desc" ref={this.desc} placeholder="Description"/>
+                <input name="image" ref={this.image} placeholder="Image" />
+                <button type="submit">Add Fish</button>
+            </form>
+        )
+    }
+}
+
+export default AddFishForm;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -2,12 +2,14 @@
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
     render() {
         return (
             <div className="inventory">
-                Inventory!!
+                <h2>Inventory</h2>
+                <AddFishForm addFish={this.props.addFish}/>
             </div>
         )
     }
