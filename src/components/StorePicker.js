418002e105d4795ce3fdd778c7cecf7d6eaf88ee
Luis Montealegre <montealegreluis@gmail.com>
2018-03-23 21:45:12 -0500

    chore: Add a random initial value to the store picker
    * Add helper functions file

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,14 @@
  * This source file is subject to the license that is bundled with this package in the file LICENSE.
  */
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
     render() {
         return (
             <form className="store-selector">
                 <h2>Please enter a Store</h2>
-                <input required placeholder="Store name"/>
+                <input required placeholder="Store name" defaultValue={getFunName()}/>
                 <button type="submit">Visit store</button>
             </form>
         )
