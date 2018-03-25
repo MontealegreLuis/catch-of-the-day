09b958fc34de30b4c4bf276836c5d346510869c9
Luis Montealegre <montealegreluis@gmail.com>
2018-03-24 19:50:05 -0500

    chore: Persist the inventory using firebase

---
+++ src/base.js
@@ -0,0 +1,17 @@
+/*
+ * This source file is subject to the license that is bundled with this package in the file LICENSE.
+ */
+import Rebase from 're-base';
+import firebase from 'firebase';
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "",
+    authDomain: "catch-of-the-day-luism.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-luism.firebaseio.com",
+});
+
+const base = Rebase.createClass(firebaseApp.database());
+
+export {firebaseApp};
+
+export default base;
